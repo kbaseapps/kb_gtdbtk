d3ab38ca643e9af13f51eedc1254f4cb7da267a2
Paramvir Dehal <psdehal@lbl.gov>
2019-04-14 19:53:50 -0700

    fix str bug

--- lib/kb_gtdbtk/utils/gtdbtk_utils.py
+++ lib/kb_gtdbtk/utils/gtdbtk_utils.py
@@ -28,7 +28,7 @@ class GTDBTkUtils():
         for path in (os.path.join(out_dir, 'gtdbtk.ar122.summary.tsv'), os.path.join(out_dir, 'gtdbtk.bact120.summary.tsv')):
             try:
                 summary_file = open(path, 'r')
-                output.append(summary_file.read())
+                output = output + summary_file.read()
                 summary_file.close()
             except:
                 pass
