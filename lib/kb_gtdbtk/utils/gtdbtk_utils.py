054f655b5e988dd4c2aacc48586f10852010802a
Paramvir Dehal <psdehal@lbl.gov>
2019-04-15 18:09:10 -0700

    logging

--- lib/kb_gtdbtk/utils/gtdbtk_utils.py
+++ lib/kb_gtdbtk/utils/gtdbtk_utils.py
@@ -13,7 +13,8 @@ class GTDBTkUtils():
         self.callback_url = callback_url
         self.cpus = cpus
         self.gtdbtk = '/bin/bash -c "source activate py2 && GTDBTK_DATA_PATH=/data gtdbtk'
-        logger = logging.getLogger(__name__)
+        logging.basicConfig(format='%(created)s %(levelname)s: %(message)s',
+                            level=logging.INFO)
 
     def gtdbtk_classifywf(self, fasta_paths):
         '''
@@ -23,9 +24,9 @@ class GTDBTkUtils():
         gtdbtk_cmd = " ".join([self.gtdbtk, "classify_wf", "--out_dir", out_dir,
                               "--genome_dir", self.shared_folder, "-x", "fa",
                                "--cpus", str(self.cpus), '"'])
-        logger.info("Starting Command:\n" + gtdbtk_cmd)
+        logging.info("Starting Command:\n" + gtdbtk_cmd)
         output = subprocess.check_output(gtdbtk_cmd, shell=True).decode('utf-8')
-        logger.info(output)
+        logging.info(output)
 
         for path in (os.path.join(out_dir, 'gtdbtk.ar122.summary.tsv'),
                      os.path.join(out_dir, 'gtdbtk.bact120.summary.tsv')):
@@ -34,6 +35,6 @@ class GTDBTkUtils():
                 output = output + summary_file.read()
                 summary_file.close()
             except Exception as exc:
-                logger.info(exc)
+                logging.info(exc)
 
         return output
