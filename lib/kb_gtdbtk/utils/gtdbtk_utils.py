8fce5ed658cf083b22735ee93d66171ba836a2cc
Paramvir Dehal <psdehal@lbl.gov>
2019-04-15 18:01:26 -0700

    more logging

--- lib/kb_gtdbtk/utils/gtdbtk_utils.py
+++ lib/kb_gtdbtk/utils/gtdbtk_utils.py
@@ -1,5 +1,6 @@
 import os
 import subprocess
+import logging
 
 
 class GTDBTkUtils():
@@ -12,6 +13,7 @@ class GTDBTkUtils():
         self.callback_url = callback_url
         self.cpus = cpus
         self.gtdbtk = '/bin/bash -c "source activate py2 && GTDBTK_DATA_PATH=/data gtdbtk'
+        logger = logging.getLogger(__name__)
 
     def gtdbtk_classifywf(self, fasta_paths):
         '''
@@ -21,9 +23,9 @@ class GTDBTkUtils():
         gtdbtk_cmd = " ".join([self.gtdbtk, "classify_wf", "--out_dir", out_dir,
                               "--genome_dir", self.shared_folder, "-x", "fa",
                                "--cpus", str(self.cpus), '"'])
-        print("Starting Command:\n", gtdbtk_cmd)
+        logger.info("Starting Command:\n" + gtdbtk_cmd)
         output = subprocess.check_output(gtdbtk_cmd, shell=True).decode('utf-8')
-        print(output)
+        logger.info(output)
 
         for path in (os.path.join(out_dir, 'gtdbtk.ar122.summary.tsv'),
                      os.path.join(out_dir, 'gtdbtk.bact120.summary.tsv')):
@@ -32,6 +34,6 @@ class GTDBTkUtils():
                 output = output + summary_file.read()
                 summary_file.close()
             except Exception as exc:
-                logging.info(exc)
+                logger.info(exc)
 
         return output
