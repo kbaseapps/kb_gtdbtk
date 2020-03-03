0429f16ef13a28c9398f3de8c377cb2ac1af0876
Gavin <gaprice@lbl.gov>
2020-03-02 16:07:22 -0800

    Have the GTDB-tk runner deal with unique IDs
    No reason for the downloader to do it, just complicates glue code and data
    structures.
    
    Next remove from downloader.

--- lib/kb_gtdbtk/core/gtdbtk_runner.py
+++ lib/kb_gtdbtk/core/gtdbtk_runner.py
@@ -4,6 +4,7 @@ Run GTDB-tk against a set of sequence files.
 
 import logging
 import json
+import os
 import pandas as pd
 import tempfile
 
@@ -14,7 +15,7 @@ from typing import Dict, List, Callable
 
 def run_gtdbtk(
         gtdbtk_runner: Callable[[List[str]], None],
-        sequences: Dict[str, Dict[str, str]],
+        sequences: Dict[Path, str],
         output_dir: Path,
         temp_dir: Path,
         min_perc_aa: float,
@@ -27,10 +28,8 @@ def run_gtdbtk(
 
     :param gtdbtk_runner: a callable that takes a list of arguments for GTDB-tk and
         excutes the program with those arguments.
-    :param sequences: Information about the fasta files. A mapping from a unique, filename safe
-        ID for each file to another mapping containing the keys 'path', which maps to the path
-        to the FASTA file, and 'assembly_name', which is the name of the assembly to use in
-        the ouput.
+    :param sequences: Information about the fasta files. A mapping from a file path to a display
+        name for the file, often simply the file name.
     :param output_dir: an extant directory in which to place the output. The output is JSON
         versions of GTDB-tk's TSV output using the 'assembly_name' from the sequences map
         as the sequence name.
@@ -41,21 +40,26 @@ def run_gtdbtk(
     '''
     # TODO input checking
     # TODO test logging, need to install an interceptor. Tested manually for now
-    # TODO disallow numbers in string form as IDs - pandas will load it as a number -> KeyError
     # TODO FOR REAL abstract the unique ID part here rather than in the downloader
 
     # all this complication is due to GTDB-tk choking on many legal, but uncommon, file name
     # characters such as |. Essentially here we provide safe file names and identitifers
     # (which GTDB-tk will use to create temporary files) and then remap to the original,
     # potentially unsafe names.
+    temp_links = temp_dir / 'links'
+    temp_links.mkdir(parents=True, exist_ok=True)
+    id_to_name = {}
     with tempfile.NamedTemporaryFile(
             mode='w',
             prefix='gtdb_tk_file_input_',
             suffix='.tmp',
             delete=False,
             dir=temp_dir) as tf:
-        for id_ in sorted(sequences):
-            tf.write(sequences[id_]['path'] + '\t' + id_ + '\n')
+        for i, path in enumerate(sorted(sequences)):
+            id_ = f'id{i}'
+            id_to_name[id_] = sequences[path]
+            os.symlink(path, temp_links / id_)
+            tf.write(str(temp_links / id_) + '\t' + id_ + '\n')
 
     temp_output = temp_dir / 'output'
     temp_output.mkdir(parents=True, exist_ok=True)
@@ -70,10 +74,10 @@ def run_gtdbtk(
 
     logging.info('Starting Command:\n' + ' '.join(gtdbtk_cmd))
     gtdbtk_runner(gtdbtk_cmd)
-    _process_output_files(temp_output, output_dir, sequences)
+    _process_output_files(temp_output, output_dir, id_to_name)
 
 
-def _process_output_files(temp_output, out_dir, sequences):
+def _process_output_files(temp_output, out_dir, id_to_name):
 
     for file_ in ('gtdbtk.ar122.summary.tsv',
                   'gtdbtk.bac120.summary.tsv',
@@ -94,7 +98,7 @@ def _process_output_files(temp_output, out_dir, sequences):
             sj = json.loads(summary_json)
             for item in sj['data']:
                 key = 'Name' if 'Name' in item else 'user_genome'
-                item[key] = sequences[item[key]]['assembly_name']
+                item[key] = id_to_name[item[key]]
 
             with open(outfile, 'w') as out:
                 out.write(json.dumps(sj))
