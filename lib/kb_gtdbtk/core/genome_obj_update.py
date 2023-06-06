314859907319e880ab3d7485c3123457e853b893
dcchivian <dcchivian@lbl.gov>
2023-06-06 22:44:05 +0000

    removed redundant code and improved style

--- lib/kb_gtdbtk/core/genome_obj_update.py
+++ lib/kb_gtdbtk/core/genome_obj_update.py
@@ -12,6 +12,8 @@ from kb_gtdbtk.core.kb_client_set import KBClients
  WORKSPACE_I, CHSUM_I, SIZE_I, META_I] = range(11)  # object_info tuple    
 
 
+# check_obj_type_genome()
+#
 def check_obj_type_genome(
         obj_type: str
         ):
@@ -37,6 +39,8 @@ def check_obj_type_genome(
         return False
 
 
+# check_obj_type_assembly()
+#
 def check_obj_type_assembly(
         obj_type: str
         ):
@@ -61,6 +65,8 @@ def check_obj_type_assembly(
         return False
 
 
+# update_genome_assembly_objs_class()
+#
 def update_genome_assembly_objs_class(
         upa: str,
         classification: Dict[str, str],
@@ -86,9 +92,7 @@ def update_genome_assembly_objs_class(
 
     top_obj = clients.dfu().get_objects({'object_refs': [upa]})['data'][0]
     # normalize upa just in case it's a ref vs. an upa
-    upa = '/'.join([str(top_obj['info'][WSID_I]),
-                    str(top_obj['info'][OBJID_I]),
-                    str(top_obj['info'][VERSION_I])])
+    upa = upa_from_info(top_obj['info'])
     obj_data = top_obj['data']
     obj_type = top_obj['info'][TYPE_I].split('-')[0]
 
@@ -102,7 +106,7 @@ def update_genome_assembly_objs_class(
         else:
             raise ValueError(f'{obj_type} type is not supported')
 
-        return _process_genome_objs(top_obj, upa, upas, classification, overwrite_tax, gtdb_ver, taxon_assignment_field, clients)
+        return process_genome_objs(top_obj, upa, upas, classification, overwrite_tax, gtdb_ver, taxon_assignment_field, clients)
 
     elif check_obj_type_assembly (obj_type):
         if 'KBaseSets.AssemblySet' == obj_type:
@@ -112,13 +116,15 @@ def update_genome_assembly_objs_class(
         else:
             raise ValueError(f'{obj_type} type is not supported')
 
-        return _process_assembly_objs(top_obj, upa, upas, classification, overwrite_tax, gtdb_ver, taxon_assignment_field, clients)
+        return process_assembly_objs(top_obj, upa, upas, classification, overwrite_tax, gtdb_ver, taxon_assignment_field, clients)
 
     else:
         raise ValueError(f'{obj_type} type is not supported')
 
 
-def _process_genome_objs(top_obj, upa, upas, classification, overwrite_tax, gtdb_ver, taxon_assignment_field, clients):
+# process_genome_objs()
+#
+def process_genome_objs(top_obj, upa, upas, classification, overwrite_tax, gtdb_ver, taxon_assignment_field, clients):
     objects_created = []
     updated_genome_refs = dict()
     genomeset_query = False
@@ -148,40 +154,13 @@ def _process_genome_objs(top_obj, upa, upas, classification, overwrite_tax, gtdb
             updated_genome_refs[genome_upa] = genome_upa
             continue
         
-        this_classification = classification[assembly_name]
-
         # set std_lineage GTDB field in genome and assembly objs
-        #  Note: sometimes lineage does not resolve species, genus, etc.
-        #        in this case the string is 's__', 'g__', etc.
-        #        for taxon_id we want the first resolved taxon level, hence the skip if len==3
-        #
-        this_taxon_id = None
-        for taxon_id in reversed(this_classification.split(';')):
-            if len(taxon_id) == 3:
-                continue
-            this_taxon_id = taxon_id
-            break
-        if this_taxon_id == None:
-            raise ValueError ("unable to fund taxon+id for {}".format(this_classification))
+        this_classification = classification[assembly_name]
+        this_taxon_id = get_taxon_id (this_classification)
+        std_lineages = get_std_lineages (this_classification, gtdb_ver, this_taxon_id)
 
-        std_lineages = { 'gtdb': { 'lineage': this_classification,
-                                   'source_ver': gtdb_ver,
-                                   'taxon_id': this_taxon_id
-                                   #'source_id': None
-                                   }
-                         }
         # update and save assembly and give genome obj new assembly upa
-        assembly_obj['data']['std_lineages'] = std_lineages
-        this_wsid = genome_obj['info'][WSID_I]
-        updated_assembly_obj_info = clients.dfu().save_objects(
-            { 'id': this_wsid,
-              'objects': [{ 'type': 'KBaseGenomeAnnotations.Assembly',
-                            'name': assembly_name,
-                            'data': assembly_obj['data']
-                            }]})[0]
-        new_assembly_ref = '/'.join([str(updated_assembly_obj_info[WSID_I]),
-                                     str(updated_assembly_obj_info[OBJID_I]),
-                                     str(updated_assembly_obj_info[VERSION_I])])
+        new_assembly_ref = update_and_save_assembly (assembly_obj, std_lineages)
 
         # update genome obj with std_lineages and new assembly obj ref
         genome_obj['data']['assembly_ref'] = new_assembly_ref
@@ -208,15 +187,7 @@ def _process_genome_objs(top_obj, upa, upas, classification, overwrite_tax, gtdb
         this_wsid = genome_obj['info'][WSID_I]
         if genomeset_query:
             this_wsid = top_wsid
-        updated_obj_info = clients.dfu().save_objects(
-            { 'id': this_wsid,
-              'objects': [{ 'type': 'KBaseGenomes.Genome',
-                            'name': genome_name,
-                            'data': genome_obj['data']
-                            }]})[0]
-        new_ref = '/'.join([str(updated_obj_info[WSID_I]),
-                            str(updated_obj_info[OBJID_I]),
-                            str(updated_obj_info[VERSION_I])])
+        new_ref = save_genome_obj (this_wsid, genome_name, genome_obj['data'])
         updated_genome_refs[original_upa] = new_ref
         desc = 'Taxonomy unchanged, taxon_assignment added GTDB'
         if this_genome_tax_written:
@@ -226,61 +197,18 @@ def _process_genome_objs(top_obj, upa, upas, classification, overwrite_tax, gtdb
         
     # update refs in genomeset
     if genomeset_query:
-        new_genomeset_data = dict()
-        genomeset_name = genomeset_obj['info'][NAME_I]
-        genomeset_type = genomeset_obj['info'][TYPE_I].split('-')[0]
-
-        if genomeset_type == 'KBaseSets.GenomeSet':
-            if genomeset_obj['data'].get('description'):
-                new_genomeset_data['description'] = genomeset_obj['data']['description']
-            else:
-                new_genomeset_data['description'] = ''
-
-            items = []
-            for gsi in obj_data['data']['items']:
-                label = ''
-                if gsi.get('label'):
-                    label = gsi['label']
-                if not updated_genome_refs.get(gsi['ref']):
-                    raise ValueError ('unable to find '+gsi['ref']+' in updated_genome_refs')
-                new_ref = updated_genome_refs[gsi['ref']]
-                items.append({'label': label, 'ref': new_ref})
-            new_genomeset_data['items'] = items
-        elif genomeset_type == 'KBaseSearch.GenomeSet':
-            if genomeset_obj['data'].get('description'):
-                new_genomeset_data['description'] = genomeset_obj['data']['description']
-            else:
-                new_genomeset_data['description'] = ''
-            
-            elements = dict()
-            old_elements = genomeset_obj['data']['elements']
-            for genome_id in old_elements.keys():
-                if not updated_genome_refs.get(old_elements[genome_id]['ref']):
-                    raise ValueError ('unable to find '+old_elements[genome_id]['ref']+' in updated_genome_refs')
-                new_ref = updated_genome_refs[old_elements[genome_id]['ref']]
-                elements[genome_id] = {'ref': new_ref}
-            new_genomeset_data['elements'] = elements
-        else:
-            raise ValueError(f'{genomeset_type} type is not supported')
-
-        updated_obj_info = clients.dfu().save_objects(
-            { 'id': top_wsid,
-              'objects': [{ 'type': 'KBaseSearch.GenomeSet',
-                            'name': genomeset_name,
-                            'data': new_genomeset_data
-              }]})[0]
-        new_ref = '/'.join([str(updated_obj_info[WSID_I]),
-                            str(updated_obj_info[OBJID_I]),
-                            str(updated_obj_info[VERSION_I])])
+        new_genomeset_ref = update_and_save_genomeset (top_wsid, genomeset_obj, updated_genome_refs)
         desc = 'Taxonomy unchanged, taxon_assignment added GTDB'
         if any_genome_updated:
             desc = 'Taxonomy and taxon_assignment updated with GTDB'
-        objects_created.append({'ref': new_ref, 'description': desc})
+        objects_created.append({'ref': new_genomeset_ref, 'description': desc})
         
     return objects_created
 
 
-def _process_assembly_objs(top_obj, upa, upas, classification, overwrite_tax, gtdb_ver, taxon_assignment_field, clients):
+# process_assembly_objs()
+#
+def process_assembly_objs(top_obj, upa, upas, classification, overwrite_tax, gtdb_ver, taxon_assignment_field, clients):
     objects_created = []
     updated_assembly_refs = dict()
     assemblyset_query = False
@@ -309,37 +237,14 @@ def _process_assembly_objs(top_obj, upa, upas, classification, overwrite_tax, gt
         else:
             any_assembly_updated = True
         
-        this_classification = classification[assembly_name]
 
         # set std_lineage GTDB field in assembly obj
-        this_taxon_id = None
-        for taxon_id in reversed(this_classification.split(';')):
-            if len(taxon_id) == 3:
-                continue
-            this_taxon_id = taxon_id
-            break
-        if this_taxon_id == None:
-            raise ValueError ("unable to fund taxon+id for {}".format(this_classification))
+        this_classification = classification[assembly_name]
+        this_taxon_id = get_taxon_id (this_classification)
+        std_lineages = get_std_lineages (this_classification, gtdb_ver, this_taxon_id)
 
-        std_lineages = { 'gtdb': { 'lineage': this_classification,
-                                   'source_ver': gtdb_ver,
-                                   'taxon_id': this_taxon_id
-                                   #'source_id': None
-                                   }
-                         }
         # update and save assembly
-        assembly_obj['data']['std_lineages'] = std_lineages
-        this_wsid = assembly_obj['info'][WSID_I]
-        updated_assembly_obj_info = clients.dfu().save_objects(
-            { 'id': this_wsid,
-              'objects': [{ 'type': 'KBaseGenomeAnnotations.Assembly',
-                            'name': assembly_name,
-                            'data': assembly_obj['data']
-                            }]})[0]
-        new_assembly_ref = '/'.join([str(updated_assembly_obj_info[WSID_I]),
-                                     str(updated_assembly_obj_info[OBJID_I]),
-                                     str(updated_assembly_obj_info[VERSION_I])])
-        
+        new_assembly_ref = update_and_save_assembly (assembly_obj, std_lineages)
         updated_assembly_refs[original_upa] = new_assembly_ref
         desc = 'Added GTDB lineage'
         objects_created.append({'ref': new_assembly_ref, 'description': desc})
@@ -347,40 +252,167 @@ def _process_assembly_objs(top_obj, upa, upas, classification, overwrite_tax, gt
         
     # update refs in assemblyset
     if assemblyset_query and any_assembly_updated:
-        new_assemblyset_data = dict()
-        assemblyset_name = assemblyset_obj['info'][NAME_I]
-        assemblyset_type = assemblyset_obj['info'][TYPE_I].split('-')[0]
-
-        if assemblyset_type == 'KBaseSets.AssemblySet':
-            if assemblyset_obj['data'].get('description'):
-                new_assemblyset_data['description'] = assemblyset_obj['data']['description']
-            else:
-                new_assemblyset_data['description'] = ''
-
-            items = []
-            for asi in assemblyset_obj['data']['items']:
-                label = ''
-                if asi.get('label'):
-                    label = asi['label']
-                if not updated_assembly_refs.get(asi['ref']):
-                    raise ValueError ('unable to find '+asi['ref']+' in updated_assembly_refs')
-                new_ref = updated_assembly_refs[asi['ref']]
-                items.append({'label': label, 'ref': new_ref})
-            new_assemblyset_data['items'] = items
-        else:
-            raise ValueError(f'{assemblyset_type} type is not supported')
-
-        # save updated assembly set obj
-        updated_obj_info = clients.dfu().save_objects(
-            { 'id': top_wsid,
-              'objects': [{ 'type': 'KBaseSets.AssemblySet',
-                            'name': assemblyset_name,
-                            'data': new_assemblyset_data
-              }]})[0]
-        new_ref = '/'.join([str(updated_obj_info[WSID_I]),
-                            str(updated_obj_info[OBJID_I]),
-                            str(updated_obj_info[VERSION_I])])
+        new_assemblyset_ref = update_and_save_assemblyset (top_wsid,
+                                                           assemblyset_obj,
+                                                           updated_assembly_refs)
         desc = 'Added GTDB lineage'
-        objects_created.append({'ref': new_ref, 'description': desc})
+        objects_created.append({'ref': new_assemblyset_ref, 'description': desc})
         
     return objects_created
+
+
+# get_taxon_id ()
+#
+def get_taxon_id (this_classification):
+    #  Note: sometimes lineage does not resolve species, genus, etc.
+    #        in this case the string is 's__', 'g__', etc.
+    #        for taxon_id we want the first resolved taxon level, hence the skip if len==3
+
+    this_taxon_id = None
+    for taxon_id in reversed(this_classification.split(';')):
+        if len(taxon_id) == 3:
+            continue
+        this_taxon_id = taxon_id
+        break
+    if this_taxon_id == None:
+        raise ValueError ("unable to fund taxon+id for {}".format(this_classification))
+
+    return this_taxon_id
+
+    
+# get_std_lineages ()
+#
+def get_std_lineages (this_classification, gtdb_ver, this_taxon_id):
+    return { 'gtdb': { 'lineage': this_classification,
+                       'source_ver': gtdb_ver,
+                       'taxon_id': this_taxon_id
+                       #'source_id': None
+                     }
+    }
+
+
+# save_genome_obj ()
+#
+def save_genome_obj (this_wsid, genome_name, genome_obj_data):
+    updated_obj_info = clients.dfu().save_objects(
+        { 'id': this_wsid,
+          'objects': [{ 'type': 'KBaseGenomes.Genome',
+                        'name': genome_name,
+                        'data': genome_obj_data
+          }]})[0]
+    new_ref = upa_from_info(updated_obj_info)
+    return new_ref
+
+
+# update_and_save_assembly ()
+#
+def update_and_save_assembly (assembly_obj, std_lineages):
+    assembly_obj['data']['std_lineages'] = std_lineages
+    this_wsid = assembly_obj['info'][WSID_I]
+    assembly_name = assembly_obj['info'][NAME_I]
+
+    updated_assembly_obj_info = clients.dfu().save_objects({ 'id': this_wsid,
+                                                             'objects': [{ 'type': 'KBaseGenomeAnnotations.Assembly',
+                                                                           'name': assembly_name,
+                                                                           'data': assembly_obj['data']
+                                                             }]})[0]
+    new_assembly_ref = upa_from_info(updated_assembly_obj_info)
+    return new_assembly_ref
+
+
+# update_and_save_genomeset ()
+#
+def update_and_save_genomeset (top_wsid, genomeset_obj, updated_genome_refs):
+    new_genomeset_data = dict()
+    genomeset_name = genomeset_obj['info'][NAME_I]
+    genomeset_type = genomeset_obj['info'][TYPE_I].split('-')[0]
+
+    # repoint to new versions of genome objs
+    if genomeset_type == 'KBaseSets.GenomeSet':
+        if genomeset_obj['data'].get('description'):
+            new_genomeset_data['description'] = genomeset_obj['data']['description']
+        else:
+            new_genomeset_data['description'] = ''
+
+        items = []
+        for gsi in genomeset_obj['data']['items']:
+            label = ''
+            if gsi.get('label'):
+                label = gsi['label']
+            if not updated_genome_refs.get(gsi['ref']):
+                raise ValueError ('unable to find '+gsi['ref']+' in updated_genome_refs')
+            new_ref = updated_genome_refs[gsi['ref']]
+            items.append({'label': label, 'ref': new_ref})
+        new_genomeset_data['items'] = items
+    elif genomeset_type == 'KBaseSearch.GenomeSet':
+        if genomeset_obj['data'].get('description'):
+            new_genomeset_data['description'] = genomeset_obj['data']['description']
+        else:
+            new_genomeset_data['description'] = ''
+            
+        elements = dict()
+        old_elements = genomeset_obj['data']['elements']
+        for genome_id in old_elements.keys():
+            if not updated_genome_refs.get(old_elements[genome_id]['ref']):
+                raise ValueError ('unable to find '+old_elements[genome_id]['ref']+' in updated_genome_refs')
+            new_ref = updated_genome_refs[old_elements[genome_id]['ref']]
+            elements[genome_id] = {'ref': new_ref}
+        new_genomeset_data['elements'] = elements
+    else:
+        raise ValueError(f'{genomeset_type} type is not supported')
+
+    # save updated genome set obj
+    updated_obj_info = clients.dfu().save_objects(
+        { 'id': top_wsid,
+          'objects': [{ 'type': 'KBaseSearch.GenomeSet',
+                        'name': genomeset_name,
+                        'data': new_genomeset_data
+          }]})[0]
+    new_ref = upa_from_info(updated_obj_info)
+    return new_ref
+
+
+# update_and_save_assemblyset ()
+#
+def update_and_save_assemblyset (top_wsid, assemblyset_obj, updated_assembly_refs):
+    new_assemblyset_data = dict()
+    assemblyset_name = assemblyset_obj['info'][NAME_I]
+    assemblyset_type = assemblyset_obj['info'][TYPE_I].split('-')[0]
+
+    # repoint to new versions of assembly objs
+    if assemblyset_type == 'KBaseSets.AssemblySet':
+        if assemblyset_obj['data'].get('description'):
+            new_assemblyset_data['description'] = assemblyset_obj['data']['description']
+        else:
+            new_assemblyset_data['description'] = ''
+
+        items = []
+        for asi in assemblyset_obj['data']['items']:
+            label = ''
+            if asi.get('label'):
+                label = asi['label']
+            if not updated_assembly_refs.get(asi['ref']):
+                raise ValueError ('unable to find '+asi['ref']+' in updated_assembly_refs')
+            new_ref = updated_assembly_refs[asi['ref']]
+            items.append({'label': label, 'ref': new_ref})
+        new_assemblyset_data['items'] = items
+    else:
+        raise ValueError(f'{assemblyset_type} type is not supported')
+
+    # save updated assembly set obj
+    updated_obj_info = clients.dfu().save_objects(
+        { 'id': top_wsid,
+          'objects': [{ 'type': 'KBaseSets.AssemblySet',
+                        'name': assemblyset_name,
+                        'data': new_assemblyset_data
+          }]})[0]
+    new_ref = upa_from_info(updated_obj_info)
+    return new_ref
+
+
+# upa_from_info ()
+#
+def upa_from_info (obj_info):
+    return '/'.join([str(obj_info[WSID_I]),
+                     str(obj_info[OBJID_I]),
+                     str(obj_info[VERSION_I])])
