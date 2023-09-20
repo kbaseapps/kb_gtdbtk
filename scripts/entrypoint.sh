50c638bd92ede3009cbb9b42ef08473d2b4a91d6
dcchivian <dcchivian@lbl.gov>
2023-09-20 05:50:51 +0000

    add r214 refdata download

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -18,8 +18,10 @@ elif [ "${1}" = "async" ] ; then
   xvfb-run bash ./scripts/run_async.sh
 elif [ "${1}" = "init" ] ; then
   echo "Initialize module"
+
+  # r207 refdata
   cd /data
-  if [! -d /data/r207] ; then
+  if [ ! -d "/data/r207" ] ; then
     mkdir r207
     cd r207
 
@@ -45,8 +47,37 @@ elif [ "${1}" = "init" ] ; then
     rm ${BAC_METADATA}
   fi
 
+  # r214 refdata
   cd /data
-  if [[ -d "r207/taxonomy" && -d "r207/fastani" && -d "r207/markers" && -s "r207/ar53_metadata_r207.tsv" && -s "bac120_metadata_r207.tsv" ]] ; then
+  if [ ! -d "/data/r214" ] ; then
+    mkdir r214
+    cd r214
+
+    export GTDB_VER_INT=214
+    export GTDB_VER_FLT=214.1
+
+    echo "Getting GTDB-Tk databases"
+    export GTDB_TK_DATA_DB=gtdbtk_r${GTDB_VER_INT}_data.tar.gz
+    curl -O https://data.gtdb.ecogenomic.org/releases/release${GTDB_VER_INT}/${GTDB_VER_FLT}/auxillary_files/${GTDB_TK_DATA_DB}
+    tar xvzf ${GTDB_TK_DATA_DB} --strip 1
+    rm ${GTDB_TK_DATA_DB}
+
+    echo "Getting GTDB Archaea metadata"
+    export ARC_METADATA=ar53_metadata_r${GTDB_VER_INT}.tar.gz
+    curl -O https://data.gtdb.ecogenomic.org/releases/release${GTDB_VER_INT}/${GTDB_VER_FLT}/${ARC_METADATA}
+    tar xvzf ${ARC_METADATA}
+    rm ${ARC_METADATA}
+
+    echo "Getting GTDB Bacteria metadata"
+    export BAC_METADATA=bac120_metadata_r${GTDB_VER_INT}.tar.gz
+    curl -O https://data.gtdb.ecogenomic.org/releases/release${GTDB_VER_INT}/${GTDB_VER_FLT}/${BAC_METADATA}
+    tar xvzf ${BAC_METADATA}
+    rm ${BAC_METADATA}
+  fi
+
+  # don't repeat if refdata prepared
+  cd /data
+  if [[ -d "r207/taxonomy" && -d "r207/fastani" && -d "r207/markers" && -s "r207/ar53_metadata_r207.tsv" && -s "r207/bac120_metadata_r207.tsv" && -d "r214/taxonomy" && -d "r214/fastani" && -d "r214/markers" && -s "r214/ar53_metadata_r214.tsv" && -s "r214/bac120_metadata_r214.tsv" ]] ;  then
     touch __READY__
   else
     echo "init failed"
